f48a8a67e4d1106ea90d8dd445dfc897e34097db
TetianaUstimevko <tetianaustimevko@gmail.com>
2023-03-11 21:07:26 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,18 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
-console.log(Player);
+// console.log(Player);
+
+const iframe = document.querySelector('#vimeo-player');
+
+// Створюємо екземпляр класу
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+
+const onPlay = function (e) {
+    localStorage.setItem(CURRENT_TIME_KEY, e.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
