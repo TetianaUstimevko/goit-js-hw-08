fb94cac2ac4a37bdf949d8cd8c6bb972c609956d
TetianaUstimevko <tetianaustimevko@gmail.com>
2023-03-11 20:23:32 +0200

    add task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
 import SimpleLightbox from "simplelightbox";
+
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(SimpleLightbox);
+// console.log(SimpleLightbox);
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('afterbegin', createGalleryItemsMarkup(galleryItems));
+
+function createGalleryItemsMarkup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `<a href="${original}" class="gallery__link"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
+    }).join('');
+}
+
+// use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
